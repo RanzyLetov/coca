1e419211f242f8f442ea3a7c9ca63693679b1a3b
Виктор Летов <viktorkravcenko461@gmail.com>
2025-10-09 17:40:43 +0700

    Created contact:hero and added cnotact:partners

---
+++ src/js/contact.js
@@ -0,0 +1,10 @@
+import '../assets/scss/contact.scss';
+
+// components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+import { useFooterAccordion } from './components/dropdown';
+
+useTheme();
+useBurger();
+useFooterAccordion();
