01ee36e9d21bbf67fcec704d4f497aa6bb6534bb
Виктор Летов <viktorkravcenko461@gmail.com>
2025-10-11 15:46:19 +0700

    Created contact:get-started section

---
+++ src/js/components/contact/phone.js
@@ -0,0 +1,16 @@
+import intlTelInput from 'intl-tel-input';
+import 'intl-tel-input/build/css/intlTelInput.css';
+
+export const usePhone = () => {
+    const input = document.querySelector('#phone');
+    intlTelInput(input, {
+        initialCountry: 'auto',
+        geoIpLookup: (callback) => {
+            fetch('https://ipapi.co/json')
+                .then((res) => res.json())
+                .then((data) => callback(data.country_code))
+                .catch(() => callback('ru'));
+        },
+        // loadUtils: () => import('/intl-tel-input/js/utils'),
+    });
+};
--- src/js/contact.js
+++ src/js/contact.js
@@ -4,7 +4,9 @@ import '../assets/scss/contact.scss';
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
 import { useFooterAccordion } from './components/dropdown';
+import { usePhone } from './components/contact/phone';
 
 useTheme();
 useBurger();
 useFooterAccordion();
+usePhone();
